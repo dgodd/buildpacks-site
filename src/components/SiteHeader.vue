42e9a9f2defd2827166b8356edcf35c2d1244609
Dave Goddard <dave@goddard.id.au>
2017-04-27 19:45:47 -0400

    Copy templates in from matt and alter JS

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="bg-white bb b--black-10 tc pv3">
+    <img class="w4 w5-l" src="../assets/cf-logo.png" alt="CloudFoundry">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'SiteHeader'
+}
+</script>
